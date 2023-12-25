6ac5d616e5b5cdad5d37eb416b58a60cfcbe9ed5
= <kirklimushin@gmail.com>
2023-12-25 23:37:53 +0300

    rebased

---
+++ docs/PRED_EXPLAINABILITY.md
@@ -0,0 +1,19 @@
+# Network prediction interpretation.
+
+Detecting deepfakes have never been an easy idea, that's why
+having an understanding of which pattern network follows
+to guide it's predictions can be beneficial in first place.
+In our research, we've leveraged several techniques and 
+explained local and global image features, relevant to the dataset.
+
+### Lime (Linear Model-Augnostic Estimations)
+
+[]("./imgs/lime/lime_analysis.png")
+
+### KernelSHAP (Version of Shapley values, designed for image explanation)
+
+[]("./imgs/shap/shapley_analysis.png")
+
+
+
+
--- rest/controllers.py
+++ rest/controllers.py
@@ -1,4 +1,3 @@
-from fastapi import UploadFile 
 import logging
 from PIL import Image
 
@@ -11,7 +10,7 @@ import os
 import numpy
 import base64
 
-BATCH_SIZE = os.environ.get("BATCH_PROCESSING_SIZE", 1)
+GPU_DEVICE = "cuda"
 
 logger = logging.getLogger("controller_logger")
 file_handler = logging.FileHandler(filename="controller_logger.log")
@@ -32,7 +31,7 @@ async def predict_human_deepfake(request: Request):
     based on the showed photo
     """
     try:
-        img_bytes_string = (await request.form())['image_string']
+        img_bytes_string = (await request.form()).get("image_hash")
 
         # decodes base64 string -> load to PIL Image object -> converts to numpy representation
         img = numpy.asarray(
@@ -40,7 +39,7 @@ async def predict_human_deepfake(request: Request):
                 fp=base64.b64decode(s=img_bytes_string)
             )
         )
-        tensor_img = torch.from_numpy(img).to("cuda")
+        tensor_img = torch.from_numpy(img).to(GPU_DEVICE)
         predicted_labels = network.forward(tensor_img).cpu()
         output_label = torch.argmax(predicted_labels, dim=0)
         
