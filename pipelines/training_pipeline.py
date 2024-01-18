adb24b3d866d409698541cc51c97e3b3112a041e
= <kirklimushin@gmail.com>
2024-01-19 00:29:27 +0300

    Updates on training pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,16 +5,32 @@ import os
 import pathlib
 import numpy 
 import random
+import matplotlib.pyplot as plt
+import logging
 
 from torch.utils.tensorboard.writer import SummaryWriter
 from pipelines import train_utils as utils
 
 from src.models import models 
-from src.losses import losses 
-from src.metrics import metrics 
+from src.losses import losses
 from src.datasets import datasets
 from src.augmentations import augmentations
 
+info_logger = logging.getLogger("pipeline_info_logger")
+err_logger = logging.getLogger("pipeline_error_logger")
+
+error_handler = logging.FileHandler(filename="pipeline_issue_logs.log")
+info_handler = logging.FileHandler(filename="pipeline_debug_logs.log")
+
+# setting up handler boundary level
+
+error_handler.setLevel(level='warning')
+info_handler.setLevel(level='info')
+
+# adding handlers
+
+err_logger.addHandler(hdlr=error_handler)
+info_logger.addHandler(hdlr=info_handler)
 
 def training_pipeline():
     """
@@ -31,46 +47,36 @@ def training_pipeline():
     arg("--checkpoint-dir", type=str, default='checkpoints/', dest='checkpoint_dir', help='path for storing training checkpoints')
     arg('--config-dir', type=str, dest='config', help='path to .json data configuration file.')
     arg("--log-dir", type=str, default='logs/', dest='log_dir', help='directory for storing logs')
-    arg("--data-size-identical", type=bool, default=False, dest='size_identical', help='In case all image data have the same size. It turns on optimization process, which speeds up training drastically')
 
     # hardware settings
     arg("--use-cuda", type=bool, default=False, dest='use_cuda', help='presence of cuda during training')
     arg("--use-cpu", type=bool, default=True, dest='use_cpu', help='presence of CPU during training')
-    arg("--num-workers", type=int, default=2, dest='num_workers', help='number of CPU threads to be involved during training')
+    arg("--num-workers", type=int, default=3, dest='num_workers', help='number of CPU threads to be involved during training')
     arg("--gpu-id", type=str, default='', help='ID of GPU to use for training')
+    arg("--use-cudnn-bench", type=str, dest='cudnn_bench', default=False, help='In case your data has the same shape, turning this option to "True" can dramatically speed up on training')
 
-    # training-specific settings
-
-    arg("--classifier-name", type=str, dest='classifier_name', help='full name of classifier model, you want to usef or training.')
-    arg("--optimizer", type=str, default='adam', help='name of the optimizer to use.')
-    arg("--lr-scheduler", type=str, default='', help='LR Scheduling technique, in format: "<name>[param1:<>,param2:<>,param3:<>]"')
-    arg("--loss-fn", type=str, dest='loss_fn', help='loss function for training classifier')
-    arg("--eval-metric", type=str, dest='eval_metric', help='evaluation metric for validating classifier')
-    arg("--max-epochs", type=int, dest='max_epochs', required=True, help='number of training epochs')
-    arg("--batch-size", type=int, dest='batch_size', default=32, help='size of the batch for training')
-
-    # regularization techniques
-
-    arg("--label-smoothing", type=bool, default=False, help='apply label smoothing')    
-    arg("--loss-weights", type=str, default='', help='loss weights for the classes')
-    arg("--weight-decay", type=float, default=1, help='constant for decaying weights during each training epoch, ')
-    arg("--loss-reduction", type=str, default='mean', choices=["mean", "sum", "none"], help='reduction technique for loss function')
-
-    arg("--early-stopping", type=bool, default=True, help='use early stopping during training or not. Default: True')
-    arg("--early-stopping-patience", default=5, help='number of bad epochs to wait, before shutting down training')
-    arg("--early-stopping-metric-diff", default=0.7, help='minimum difference between epoch metrics')
-    arg("--early-stopping-data-prop", type=float, default=0.2, help='proportion of validation data to use for early stopping')
+    # reproducability settings
 
     arg("--seed", type=int, default=42, help='seed for fixating generation of random entities during training')
     
     # experiment settings (for experiment tracking)
 
     arg("--prefix", type=str, dest='exp_prefix', help='unique prefix of the experiment. Example: 45g2srgw756')
-
+    arg("-log-execution", default=True, help='Option for use / disabling logging during pipeline execution.')
+    
     # parsing arguments
 
     args = parser.parse_args()
 
+    if (args.log_execution == False):
+
+        err_logger.disabled = True
+        info_logger.disabled = True
+    else:
+        err_logger.disabled = False 
+        info_logger.disabled = False 
+
+    info_logger.info("configurations parsed successfully")
 
     # Setting reproducability
     numpy.random.seed(args.seed)
@@ -86,43 +92,58 @@ def training_pipeline():
     config_dir = pathlib.Path(args.config_dir)
     checkpoint_dir = pathlib.Path(args.checkpoint_dir)
 
+
     log_dir.mkdir(exist_ok=True)
     output_dir.mkdir(exist_ok=True)
     checkpoint_dir.mkdir(exist_ok=True)
 
-    if (args.size_identical == True):
-        torch.backends.cudnn.benchmark = True
+    info_logger.info("recreated paths")
 
-    data_config = utils.load_config(config_dir) # data configuration (dict-like object)
+    torch.backends.cudnn.benchmark = args.cudnn_bench
 
-    train_image_paths, train_image_labels = utils.get_train_data(train_dir)
-
-    train_augmentations = augmentations.get_training_augmentations(
-        HEIGHT=data_config['image_height'], 
-        WIDTH=data_config['image_width']
-    )
+    if (args.cudnn_bench == True):
+        err_logger.warn(
+            "cunn.benchmark mode has been turned on. \
+            Make sure the data has the same size.")
     
-    val_image_paths, val_image_labels = utils.get_val_data(valid_dir)
+    # loading experiment configuration
 
-    val_augmentations = augmentations.get_validation_augmentations(
-        HEIGHT=data_config['image_height'], 
-        WIDTH=data_config['image_width']
-    )
+    try:
+        exp_config = utils.load_config(config_dir) # data configuration (dict-like object)
 
-    train_dataset = datasets.DeepfakeDataset(
-        image_paths=train_image_paths,
-        image_labels=train_image_labels,
-        augmentations=train_augmentations,
-    )
+        train_image_paths, train_image_labels = utils.get_train_data(train_dir)
 
-    validation_dataset = datasets.DeepfakeDataset(
-        image_paths=val_image_paths,
-        image_labels=val_image_labels,
-        augmentations=val_augmentations
-    )
+        train_augmentations = augmentations.get_training_augmentations(
+            HEIGHT=exp_config['data']['height'], 
+            WIDTH=exp_config['data']['width']
+        )
+        
+        val_image_paths, val_image_labels = utils.get_val_data(valid_dir)
 
-    # setting up snapshot summarty writer 
-    log_dir + "/" + args.exp_prefix
+        val_augmentations = augmentations.get_validation_augmentations(
+            HEIGHT=exp_config['data']['height'], 
+            WIDTH=exp_config['data']['width']
+        )
+    except(Exception) as config_err:
+        err_logger.error(config_err)
+
+    try:
+        # setting up training dataset for training
+        train_dataset = datasets.DeepfakeDataset(
+            image_paths=train_image_paths,
+            image_labels=train_image_labels,
+            augmentations=train_augmentations,
+        )
+        # setting up validation dataset for evaluation
+        validation_dataset = datasets.DeepfakeDataset(
+            image_paths=val_image_paths,
+            image_labels=val_image_labels,
+            augmentations=val_augmentations
+        )
+    except(Exception) as init_err:
+        err_logger.error(init_err)
+
+    # setting up snapshot summarty writer
     summary_writer = SummaryWriter(log_dir=log_dir)
 
     # setting up hardware settings
@@ -140,22 +161,37 @@ def training_pipeline():
 
             if isinstance(args.gpu_id, str) and args.gpu_id[0].isdigit():
                 device_name = "cuda:%s" % (args.gpu_id[0])
+        else:
+            err_logger.warn("you didnt provide any GPU ids. \
+            Cuda is going to leverage all available GPUs during training.")
 
     train_device = torch.device(device_name)
     num_cpu_workers = int(args.num_workers)
 
+    if num_cpu_workers < 3:
+        err_logger.warn("You've set number of cpu threads to be less, \
+        than the baseline (recommended is 3).")
+
+    elif num_cpu_workers > 5:
+        err_logger.warn("You've set number of cpu threads to be greater, \
+         than it is recommended. (5 threads is usually considered as enough'")
+
     # training-specific settings
 
-    network = utils.load_network(args.classifier_name)
-    optimizer = utils.get_optimizer(args.optimizer)
-    lr_scheduler = utils.get_lr_scheduler(args.lr_scheduler)
+    network = utils.load_network(exp_config['network'])
+    optimizer = utils.get_optimizer(exp_config['optimizer'])
+    lr_scheduler = utils.get_lr_scheduler(exp_config['scheduler']) if 'scheduler' in exp_config else None
 
-    loss_weights = torch.as_tensor(args.loss_weights.split(",")).to(torch.float16)
-    loss_function = losses.WeightedLoss(name=args.loss_fn, weights=loss_weights)
-    evaluation_metric = utils.get_evaluation_metric(args.eval_metric)
+    loss_function = losses.WeightedLoss(
+        name=exp_config['loss']['name'], 
+        weights=exp_config['loss']['weights'],
+        weight_type=exp_config['loss']['type']
+    )
 
-    batch_size = int(args.batch_size)
-    max_epochs = int(args.max_epochs)
+    evaluation_metric = utils.get_evaluation_metric(exp_config['eval_metric'])
+
+    batch_size = int(exp_config['batch_size'])
+    max_epochs = int(exp_config['max_epochs'])
     
     # setting up classifier training pipeline 
 
@@ -170,10 +206,36 @@ def training_pipeline():
         lr_scheduler=lr_scheduler,
         checkpoint_dir=checkpoint_dir,
         output_weights_dir=output_dir,
-        summary_writer=summary_writer,
         loader_num_workers=num_cpu_workers
     )
 
     # training classifier
-    _, _ = trainer.train(train_dataset=train_dataset)
+    training_loss, loss_history = trainer.train(train_dataset=train_dataset)
 
+    # plot of the loss function change during training
+
+    loss_figure = plt.figure(figsize=(30, 30))
+    _, ax = plt.plot(loss_history)
+
+    ax.axes.set_xticklabels(
+        labels=[
+            'epoch %s' % str(epoch) 
+            for epoch in 
+            range(len(loss_history))
+        ]
+    )
+
+    summary_writer.add_figure(tag='train loss history', figure=loss_figure)
+    summary_writer.add_scalar(tag='training loss', scalar_value=training_loss)
+
+    # evaluating classifier on entire validation dataset
+    evaluation_metric = trainer.evaluate(
+        validation_dataset=validation_dataset
+    )
+    # saving evaluation metric results to summary writer
+    summary_writer.add_scalar(
+        "evaluation metric: ", 
+        scalar_value=evaluation_metric, 
+        double_precision=True
+    )
+    print('training completed.')
