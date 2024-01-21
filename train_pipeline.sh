9638edfb87e79d454d04449224154e2ef8dbe451
= <kirklimushin@gmail.com>
2024-01-21 16:23:13 +0300

    Updated shell script for running train pipeline

--- train_pipeline.sh
+++ train_pipeline.sh
@@ -7,23 +7,23 @@
 # Usage Guide
 # 1. run source <path-to-env-file> in order to export configuration. 
 # Make sure you've set up all variables, specified in the bellow command.
-# 2. run this shell script using 'sh train_pipeline.sh'.s
+# 2. run this shell script using 'sh train_pipeline.sh'.
 
-python3 -u -m src.pipelines.train_pipeline.py \
+python3 -u -m src.pipelines.train_pipeline \
  --train-data-path $TRAIN_DATA_PATH \
+ --train-labels-path $TRAIN_LABELS_PATH \
  --val-data-path $VAL_DATA_PATH \
- --labels-path $LABELS_PATH \
+ --val-labels-path $VAL_LABELS_PATH \
  --output-path $OUTPUT_PATH \
  --config-path $CONFIG_PATH \
  --checkpoint-path $CHECKPOINT_PATH \
  --log-path $LOG_PATH \
- --use-cuda $ENABLE_CUDA \
  --use-cpu $ENABLE_CPU \
+ # --use-cuda $ENABLE_CUDA \
+ # --use-mps $ENABLE_MPS \
  --num-workers $NUM_CPU_WORKERS \
  --gpu-id $GPU_ID \
  --use-cudnn-bench $ENABLE_CUDNN_BENCH \
  --seed $SEED \
- --prefix $EXP_PREFIX \
- --log-execution $ENABLE_LOGGING
-
+ --log-execution $ENABLE_LOGGING \
 
