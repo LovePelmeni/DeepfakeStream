bc45f388d43e5a6886c62f41224640d6bb9f9bbb
= <kirklimushin@gmail.com>
2024-01-22 19:06:20 +0300

    Automated data configuration loading for preproc pipeline. only thing you need to do is specify mode: 'train' or 'validation', when calling: 'sh preproc_pipeline.sh' as a first argument

--- preproc_pipeline.sh
+++ preproc_pipeline.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/sh
 
 # Shell script
 # for automating data preprocessing
@@ -9,11 +9,38 @@
 # 3. DATA_DIR - full path to your dataset.
 # 4. AUGMENTED_DATA_DIR - full path, where your augmented data is going to be stored after preprocessing
 
+MODE=$1
+
+if [ "$MODE" == "train" ];
+then 
+    FILE=./env_vars/train_data_pipeline.env
+    if [ -f "$FILE" ]; 
+    then source $FILE
+    else echo "$FILE does not exist.\n
+    Shell script loads configuration for training dataset from this file.\n
+    Before usage, you need to create this file and set up configuration for your training data.\n
+    For more details, see 'docs/data_management/DATA_PIPELINE.md'."
+    fi;
+elif [ "$MODE" == "validation" ];
+then 
+    FILE=./env_vars/val_data_pipeline.env
+    if [ -f "$FILE" ]; 
+    then source $FILE
+    else echo "$FILE does not exist.\n
+    Shell script loads configuration for validation dataset from this file.\n
+    Before usage, you need to create this file and set up configuration for your validation data.\n
+    For more details, see 'docs/data_management/DATA_PIPELINE.md'."
+    fi;
+else 
+    echo "Invalid MODE parameter provided: should be either 'train' or 'validation'. \n
+    'train' means, it will load .env configuration for the training dataset, \n
+    provided under 'env_vars/train_data_pipeline.env' path. 'validation' means, it will load .env configuration for validation dataset \
+    provided under 'env_vars/val_data_pipeline.env' path. You can use only these options, nothing else. \n
+    For more details check 'docs/data_management/DATA_PIPELINE.md'."
+    exit 1;
+fi
+
 python3 -u -m src.pipelines.preproc_pipeline \
  --config-dir $DATA_CONFIG_DIR \
  --data-dir $DATA_DIR \
  --output-dir $AUGMENTED_DATA_DIR
-
-
-
-
