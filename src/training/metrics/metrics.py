94659f2e82b58707c55fd2ba4f3a4621b3ea7c27
= <kirklimushin@gmail.com>
2024-02-02 20:58:44 +0300

    Added Implementation of  Average Precision evaluation metric. Closes #25

--- src/training/metrics/metrics.py
+++ src/training/metrics/metrics.py
@@ -49,3 +49,63 @@ class IOUScore(nn.Module):
         union_area = pred_area + act_area
         intersection_area = (xmax - xmin) * (ymax - ymin)
         return intersection_area / union_area + self.eps
+
+
+class AveragePrecision(nn.Module):
+    """
+    Implementation of AP (Average Precision)
+    Evaluation Metric, commonly used for evaluating
+    video object detection systems
+    """
+    def __init__(self, 
+        total_classes: int,
+        conf_threshold: float = 0.5, 
+        iou_threshold: float = 0.5,
+    ):
+        super(AveragePrecision, self).__init__()
+
+        self.conf_threshold = conf_threshold
+        self.iou_threshold = iou_threshold
+        self.total_classes = total_classes
+        self.iou = IOUScore()
+
+    def forward(self, 
+        pred_boxes: typing.Dict[str, typing.Dict], 
+        actual_boxes: typing.Dict[str, typing.Dict]
+     ): 
+        """
+        Parameters:
+        -----------
+        pred_boxes - dictionary of following format: [{'img1': {'boxes': [], 'conf_scores': []}}, ...]
+        actual_boxes - list of following objects: [{'img1': {'boxes': []}}, {'img2': {'boxes': []}}
+        """
+        
+        # matching similar boxes, based on IOU score
+
+        total_boxes = torch.sum([len(actual_boxes[sample]['boxes']) for sample in actual_boxes])
+        iou_scores = torch.zeros(total_boxes)
+
+        for img_id in pred_boxes:
+
+            pred_img_boxes = pred_boxes[img_id]
+            actual_img_boxes = actual_boxes[img_id]
+
+            for box1 in range(len(pred_img_boxes)):
+                iou_scores = [
+                    self.iou(pred_img_boxes[box1], actual_img_boxes[actual_box])
+                    for actual_box in actual_img_boxes
+                ]
+                iou_scores[box1] = torch.argmax(iou_scores, dim=0)
+                
+        # calculating true positives and false positives
+        conf_scores = torch.where(conf_scores >= self.conf_threshold, 1, 0)
+        iou_scores = torch.where(iou_scores >= self.iou_threshold, 1, 0)
+        overall_box_status = numpy.multiply(conf_scores, iou_scores)
+        
+        tp = numpy.where(overall_box_status != 0, 1, 0)
+
+        recalls = torch.cumsum(tp / torch.sum(tp))
+        precisions = torch.cumsum(tp / len(tp))
+
+        pr_area = torch.trapz(y=precisions, x=recalls)
+        return pr_area
