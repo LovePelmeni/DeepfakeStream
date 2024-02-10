57453fdb0a63cc2f5f1fb883518c232de8e2466f
= <kirklimushin@gmail.com>
2024-02-10 19:52:46 +0300

    Merged cherry-picked commit

--- src/training/classifiers/base_classifier.py
+++ src/training/classifiers/base_classifier.py
@@ -1,14 +1,24 @@
 from torch import nn
 import torch
-from abc import ABC
 from src.training.classifiers import srm_conv
+from timm import models
 
-ENCODER_CONFIGURATION = {}
+encoders = {
+    "efficientnet-b3": {
+        "cls": models.efficientnet.efficientnet_b3,
+        "features": 1280
+    },
+    "efficientnet-b2": {
+        "cls": models.efficientnet.efficientnet_b2,
+        "features": 1280,
+    },
+    "efficientnet-b4": {
+        "cls": models.efficientnet.efficientnet_b1,
+        "features": 1280
+    }
+}
 
-class BaseClassifier(ABC):
-    pass
-
-class DeepfakeClassifier(nn.Module, BaseClassifier):
+class DeepfakeClassifier(nn.Module):
     """
     Deepfake classifier prototype
     implementation, with help of research paper,
@@ -29,21 +39,37 @@ class DeepfakeClassifier(nn.Module, BaseClassifier):
         encoder: nn.Module
     ):
         super(DeepfakeClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=input_channels)
+        self.conv1 = nn.Conv2d(
+            in_channels=input_channels, 
+            out_channels=input_channels,
+            bias=False
+        ) 
         self.encoder = encoder
         self.avgpool1 = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout1 = nn.Dropout()
-        self.dense1 = nn.Linear(in_features=1280, out_features=128)
+        self.dense1 = nn.Linear(
+            in_features=encoders[encoder]['out_features'],
+            out_features=128,
+            bias=True
+        )
         self.relu1 = nn.ReLU()
-        self.dense2 = nn.Linear(in_features=128, out_features=64)
+        self.dense2 = nn.Linear(
+            in_features=128, 
+            out_features=64,
+            bias=True
+        )
         self.relu2 = nn.ReLU()
-        self.dense3 = nn.Linear(in_features=64, out_features=1)
+        self.dense3 = nn.Linear(
+            in_features=64, 
+            out_features=1,
+            bias=True
+        )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_map: torch.Tensor):
         output = self.conv1(input_map)
         output = self.encoder(output)
-        output = self.avgpool1(output)
+        output = self.avgpool1(output).flatten(1)
         output = self.dropout1(output)
         output = self.dense1(output)
         output = self.relu1(output)
@@ -91,10 +117,10 @@ class DeepfakeClassifierSRM(nn.Module):
         self.sigmoid = nn.Sigmoid()
     
     def forward(self, input_map: torch.Tensor):
-        output = self.conv1(input_map)
+        noise = self.srm_conv(input_map)
+        output = self.conv1(noise)
         output = self.encoder(output)
         output = self.avgpool1(output)
-        output = self.srm_conv(output)
         output = self.dropout1(output)
         output = self.dense1(output)
         output = self.relu1(output)
@@ -111,22 +137,83 @@ class GlobalWeightedAveragePooling(nn.Module):
     Pixel-level Localization and Image-level
     Classification by Suo Qiu"
     """
-    def __init__(self, input_size: int, in_channels: int):
+    def __init__(self, input_size: int, flatten: bool = False):
         super(GlobalWeightedAveragePooling, self).__init__()
-        self.weights = nn.Parameter(torch.nn.init.xavier_uniform_(
-            torch.empty(size=(in_channels, input_size, input_size))
-        ))
-    
-    def forward(self, input_map: torch.Tensor):
-        weighted_maps = input_map * self.weights.unsqueeze(0)
-        return torch.mean(weighted_maps, dim=(2, 3))
-        
-class DeepfakeClassifierGWAP(DeepfakeClassifier):
+        self.conv = nn.Conv2d(
+            input_size, 
+            out_channels=1, 
+            kernel_size=1, 
+            bias=False
+        )
+        self.flatten = flatten
+
+    def fscore(self, x):
+            m = self.conv(x)
+            m = m.sigmoid().exp()
+            return m
+
+    def norm(self, x: torch.Tensor):
+        return x / x.sum(dim=[2, 3], keepdim=True)
+
+    def forward(self, x):
+        input_x = x
+        x = self.fscore(x)
+        x = self.norm(x)
+        x = x * input_x
+        x = x.sum(dim=[2, 3], keepdim=not self.flatten)
+        return x
+class DeepfakeClassifierGWAP(nn.Module):
     """
     Implementation of the Deepfake classifier
     with usage of Global Weighted Average Pooling layer,
     instead of standard avg pool.
     """
-    def __init__(self, **kwargs):
-        super(DeepfakeClassifierGWAP, self).__init__(**kwargs)
-        self.avgpool1 = GlobalWeightedAveragePooling()
+    def __init__(self, encoder):
+        super(DeepfakeClassifierGWAP, self).__init__()
+
+        self.encoder = encoders[encoder]['cls']()
+
+        self.globalpool1 = GlobalWeightedAveragePooling(
+            input_size=encoders[encoder]['features'],
+            flatten=True
+        )
+
+        self.batchnorm1 = nn.BatchNorm1d(
+            num_features=encoders[encoder]['features']
+        )
+
+        self.dropout1 = nn.Dropout(p=0.5)
+
+        self.dense1 = nn.Linear(
+            in_features=encoders[encoder]['features'], 
+            out_features=64,
+            bias=True
+        )
+
+        self.relu1 = nn.ReLU()
+
+        self.dense2 = nn.Linear(
+            in_features=64,
+            out_features=1,
+            bias=True
+        )
+
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, image: torch.Tensor):
+
+        if not len(image.shape) == 4:
+            raise ValueError("""input should have 4 dimensions: 
+            (batch_size, channels, height, width), however, 
+            you passed data with %s dimensions.""" % len(image.shape))
+
+        output = self.encoder(image)
+        output = self.globalpool1(output)
+        output = self.batchnorm1(output)
+        output = self.dropout1(output)
+        output = self.dense1(output)
+        output = self.relu1(output)
+        output = self.dense2(output)
+        probs = self.sigmoid(output)
+        return probs
+
