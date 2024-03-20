ae6f78a5e47f63c19ca79a5004db8941ef4a712c
= <kirklimushin@gmail.com>
2024-03-20 20:23:24 +0300

    Added quantization modules.

---
+++ src/quantization/network_quantizer.py
@@ -0,0 +1,115 @@
+from torch import nn
+import torch.quantization
+import numpy
+import torch.ao.quantization
+from torch.utils import data
+import torch
+
+class InputQuantizer(object):
+    """
+    Inference quantizer. Application string
+    container string branch application string validator.
+    Parameters:
+    ----------
+        quantization_bits (int) - number of bits to use for
+        quantization process.
+    """
+    def __init__(self, 
+        quantization_type, 
+        quan_scale, 
+        quan_zero
+    ):
+        self.quantization_type = quantization_type
+        self.quantization_scale = quan_scale
+        self.quantization_zero = quan_zero
+
+    def __call__(self, input_img: numpy.ndarray):
+        return torch.quantize_per_tensor(
+            input_img,
+            self.quantization_scale,
+            self.quantization_zero,
+            self.quantization_type,
+        )
+
+class StaticNetworkQuantizer(object):
+    """
+    Base module for performing static quantization
+    of the network.
+    """
+    def __init__(self, quan_type):
+        self.quan_type = quan_type
+        self.calibrator = NetworkCalibrator()
+
+    def quantize(self, 
+        input_model: nn.Module, 
+        calibration_dataset: data.Dataset, 
+        calib_batch_size: int
+    ):
+        calibration_loader = self.calibrator.configure_calibration_loader(
+            calibration_dataset=calibration_dataset,
+            calibration_batch_size=calib_batch_size,
+            loader_workers=2
+        )
+        # perform calibration
+        stat_network = self.calibrator.calibrate(
+            input_model,
+            loader=calibration_loader,
+            q_type=self.quan_type
+        )
+        quantized_model = torch.quantization.convert(stat_network)
+        return quantized_model 
+
+class NetworkCalibrator(object):
+
+    """
+    Base module for performing
+    calibration for static post-training
+    quantization.
+    """
+
+    def configure_calibration_loader(self, 
+        calibration_dataset: data.Dataset,
+        calibration_batch_size: int,
+        loader_workers: int = 1
+    ):
+        """
+        Configures data loader for
+        performing calibration.
+        """
+        return data.DataLoader(
+            dataset=calibration_dataset,
+            batch_size=calibration_batch_size,
+            shuffle=True,
+            num_workers=loader_workers,
+        )
+
+    def calibrate(self, 
+        network: nn.Module, 
+        loader: data.DataLoader,
+        q_type
+    ):
+        """
+        Calibrates given network
+        for finding optimal quantization
+        parameters.
+        
+        NOTE:
+            loader should contain a dataset,
+            which is originally derived from
+            the training set.
+        """
+        network.eval()
+        # Specify the quantization configuration
+        qconfig = torch.ao.quantization.QConfig(
+            activation=torch.ao.quantization.observer.MinMaxObserver.with_args(dtype=q_type),
+            weight=torch.ao.quantization.observer.MinMaxObserver.with_args(dtype=q_type)
+        )
+        # Apply the quantization configuration to the model
+        network.qconfig = qconfig
+        stat_network = torch.ao.quantization.prepare(network)
+
+        # performing calibration 
+        for images, _ in loader:
+            stat_network.forward(images).cpu()
+
+        return stat_network
